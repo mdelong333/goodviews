4d2d64ec4608c1226cf3411643be83f577843673
Chase Laptop <james.chase.richards@gmail.com>
2019-12-13 13:47:58 -0500

    destroy

--- public/js/omdb.js
+++ public/js/omdb.js
@@ -3,12 +3,16 @@ var favorite;
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 $("#add-movie").on("click", function(event) {
   event.preventDefault();
-  console.log("click")
+  console.log("click");
 
-  var movie = $("#movie-input").val().trim();
+  var movie = $("#movie-input")
+    .val()
+    .trim();
 
   if (
-    !$("#movie-input").val().trim()
+    !$("#movie-input")
+      .val()
+      .trim()
   ) {
     alert("Enter a movie to search");
   } else {
@@ -27,7 +31,7 @@ function displayMovieInfo(movie) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
+    // console.log(response);
     movieData = {
       poster: response.Poster,
       title: response.Title,
@@ -47,11 +51,13 @@ function displayMovieInfo(movie) {
     // <button class="btn-floating btn-small waves-effect waves-light grey darken-4"><i class="far fa-heart heart red-text"></i></button><button class="waves-effect waves-light btn-small grey darken-4"><i class="fas fa-clipboard-list"></i></button>
     // </div>
     // `);
-    
+
     // Display on Modal
     // FIXME Will not display image
-    
-    $("#modal-image").html(`<img src="${movieData.poster}" alt="Poster for ${movieData.title}">`);
+
+    $("#modal-image").html(
+      `<img src="${movieData.poster}" alt="Poster for ${movieData.title}">`
+    );
     $("#modal-title").html(`${movieData.title}`);
     $("#modal-year").html(`${movieData.year}`);
     $("#modal-rating").html(`Rated: ${movieData.rating}`);
@@ -81,6 +87,12 @@ function displayMovieInfo(movie) {
       else if ($(this).html() == "favorite") {
         $(this).html("favorite_border");
         movieData.favorite = false;
+        // var numAffectedRows = await movieData.destroy({
+        //   where: {
+        //     favorite: false // deletes all pugs whose age is 7
+        //   }
+        // })
+        // console.log(numAffectedRows) // if we had 3 pugs with the age of 7, this will be 3
         // TODO remove movie to database
         console.log(movieData.title + " removed from favorites");
       }
@@ -88,9 +100,15 @@ function displayMovieInfo(movie) {
   });
 }
 
-//add to database
+// This function grabs favorite from the database and updates the view
+// function getFavorites() {
+//   $.get("/api/favorite", function(data) {
+//     movieData = data;
+//   });
+// }
+
+// add to database
 function insertFavorite(event) {
-  
   var favorite = {
     title: movieData.title,
     favorite: true
@@ -98,5 +116,16 @@ function insertFavorite(event) {
   console.log(favorite);
 
   $.post("/api/favorites", favorite);
-  title.val("");
-};
+  // title.val("");
+}
+
+// function deleteFavorite(event) {
+//   event.stopPropogation();
+//   var id = $(this).data("id");
+//   $.ajax({
+//     method: "DELETE",
+//     url: "/api/favorites/" + id
+//   });
+// }
+
+// title.find("button.delete").data("id", favorite.id);
